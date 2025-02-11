6f626094c49d68d6b1931b7f8b97ab614dde5671
rahulnikam0600 <rahulnikam0600@gmail.com>
2025-02-11 14:58:09 +0530

    Hibernate configuration file is created and We are able to create and test Factory session object

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        SessionFactory factory = new Configuration()
+        		.configure()
+        		.buildSessionFactory();
+        
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
